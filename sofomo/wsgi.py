a9b57537299cf6e7481843303cfcba82e840e520
SzymonStasiowski <sz.stasiowski2@gmail.com>
2020-01-14 23:04:03 +0100

    heroku settings

--- sofomo/wsgi.py
+++ sofomo/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sofomo.settings')
 
 application = get_wsgi_application()
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
